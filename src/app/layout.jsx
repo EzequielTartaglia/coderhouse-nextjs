12bf4f62aca25b4ca41c9b8731be18d08e9686d4
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-09-27 11:12:47 -0300

    Project 1.0

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React, { useState } from "react"; 
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "../../components/Navbar";
+
+// import firebase from 'firebase';
+import '../../lib/firebase/client/client'
+
+const inter = Inter({ subsets: ["latin"] });
+
+const navRoutes = [
+  { title: "Inicio", path: "/" },
+  { title: "Contactos", path: "/contact" },
+];
+
+
+const RootLayout = ({ children }) => {
+  // Aquí guardamos el estado de inicio de sesión
+  const [user, setUser] = useState(null);
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar navItems={navRoutes} user={user} setUser={setUser} />
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+import HomePage from "../../views/HomePage/HomePage"
+
+
+
+export default function home() {
+  return <HomePage/>
+}
